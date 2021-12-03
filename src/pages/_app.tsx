d97b534e0b621889357aa67e52ec92e48745261d
RimlTempest <riml.slime@gmail.com>
2021-12-03 13:41:25 +0900

    add: googleAnalytics対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import GoogleAnalytics from '@/components/wrapperComponent/GoogleAnalytics';
 import { theme } from '@/config/chakra';
+import usePageView from '@/hooks/usePageView';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    usePageView();
     return (
-        <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <>
+            <GoogleAnalytics />
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
     );
 }
 
